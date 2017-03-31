73bebc552704f3fb5dcee9320a62f3ab2a45b408
Seatigh <100303493@alumnos.uc3m.es>
2017-03-31 22:24:58 +0200

    V0R0C3 Adding some functionality to loops.
    Loops added, but don't work right.
    Check loops functionality.

--- src/test_inter.c
+++ src/test_inter.c
@@ -6,7 +6,9 @@
 
 #define MEM_SIZE 15
 
-int i;
+int fd, i, m_pointer, loop_counter;
+char c;
+unsigned char memory [MEM_SIZE];
 
 int isBF(char *str) {
 	if (str[strlen(str)-3] == '.' && str[strlen(str)-2] == 'b' && str[strlen(str)-1] == 'f')
@@ -14,74 +16,39 @@ int isBF(char *str) {
 	return 1;
 }
 
-void showMemory (unsigned char * mem, int p) {
+void showMemory () {
 	printf("Memory status:\n");
 	/*for (i = 0; i < MEM_SIZE; i++) {
 		printf("%d ", i);
 	}
 	printf("\n");*/
 	for (i = 0; i < MEM_SIZE; i++) {
-		printf("%c ", mem[i]);
+		printf("%c ", memory[i]);
 	}
-	/*char blanks [2*p];
-	memset(blanks, ' ', 2*p);
+	/*char blanks [2*m_pointer];
+	memset(blanks, ' ', 2*m_pointer);
 	printf("\n%s*\n", blanks);*/
 	printf("\n");
-	for (i = 0; i < p; i++) {
+	for (i = 0; i < m_pointer; i++) {
 		printf("  ");
 	}
 	printf("*\n");
 }
 
-int main (int argc, char *argv[]) {
-	int fd;
-	char c;
-	/* Init memory. This could be changed to be dynamic or argument passed */
-	unsigned char memory [MEM_SIZE];
-	int m_pointer; // Memory pointer
-
-	/********************
-	 * Check parameters *
-	 *******************/
-	if (argc != 2) {
-		fprintf(stderr, "Error: Invalid number of arguments.\nExe form:\n\t%s <.bf file>\n", argv[0]);
-		return -1;
-	}
-	if (isBF(argv[1])) {
-		fprintf(stderr, "Error: File must be a .bf file.\n");
-		return -1;
-	}
-	/********************/
-
-	/* Open and check file */
-	fd = open(argv[1], O_RDONLY);
-	printf("fd: %i\n", fd);
-	if (fd < 0) {
-		fprintf(stderr, "Error: can't open file %s or it doesn't exits.\n", argv[1]);
-		return -1;
-	}
-
-	printf("Interpret file: %s\n", argv[1]);
-
-	/***************
-	 * Init memory * // Memory could be initialliced in a dynamic way with all 0's
-	 **************/
-	memset(memory, 'a', MEM_SIZE);
-	m_pointer = 0;
-
-	/* Read file */
-	/*
-	 * >	pointer++
-	 * <	pointer--
-	 * +	*pointer++
-	 * -	*pointer--
-	 * .	print
-	 * ,	scan (i never used this
-	 * [	open while
-	 * ]	close while
-	 *
-	 */
-	//showMemory(memory, m_pointer);
+/* Read file */
+/*
+ * >	pointer++
+ * <	pointer--
+ * +	*pointer++
+ * -	*pointer--
+ * .	print
+ * ,	scan (i never used this
+ * [	open while
+ * ]	close while
+ *
+ */
+void readBF () {
+	int loop_pointer;
 	while(read(fd, &c, 1) == 1) {
 		/* Conditions to parser 2 brainfuck */
 		if (c == '>')
@@ -94,22 +61,63 @@ int main (int argc, char *argv[]) {
 			memory[m_pointer] = (memory[m_pointer] - 1) % 255;
 		else if (c == '.')
 			printf("%c", memory[m_pointer]);
-		else if (c == '[') {
+		else if (c == '[') { // CHECK THIS, IT ISN'T WORKING
 			// start while loop
+			loop_pointer = lseek(fd, 0, SEEK_CUR);
 			if (memory[m_pointer] < 1) {
 				read(fd, &c, 1);
-				while (c != ']') {
-					read (fd, &c, 1);
+				while (c != ']' && loop_counter > 0) {
+					if (c == '[')
+						loop_counter++;
 				}
+			} else {
+				readBF();
 			}
 		} else if (c == ']') {
 			// end while loop
 			if (memory[m_pointer] > 0) {
-				// move pinter back to '['
+				lseek(fd, loop_pointer, SEEK_SET);
 			}
 		}
-		//showMemory(memory, m_pointer);
+		showMemory();
 	}
+}
+
+int main (int argc, char *argv[]) {
+	/********************
+	 * Check parameters *
+	 *******************/
+	if (argc != 2) {
+		fprintf(stderr, "Error: Invalid number of arguments.\nExe form:\n\t%s <.bf file>\n", argv[0]);
+		return -1;
+	}
+	if (isBF(argv[1])) {
+		fprintf(stderr, "Error: File must be a .bf file.\n");
+		return -1;
+	}
+	/********************/
+	
+	/***********************
+	 * Open and check file *
+	 ***********************/
+	fd = open(argv[1], O_RDONLY);
+	printf("fd: %i\n", fd);
+	if (fd < 0) {
+		fprintf(stderr, "Error: can't open file %s or it doesn't exits.\n", argv[1]);
+		return -1;
+	}
+
+	printf("Interpret file: %s\n", argv[1]);
+
+	/***************
+	 * Init memory * // Memory could be initialliced in a dynamic way with all 0's
+	 **************/
+	memset(memory, 0, MEM_SIZE);
+	m_pointer = 0;
+	loop_counter = 0;
+
+	//showMemory();
+	readBF();
 	printf("\n");	
 	return 0;
 }
