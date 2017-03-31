634da1f419465cae30a373cb7ebfb133ae2b13c8
Seatigh <100303493@alumnos.uc3m.es>
2017-03-31 12:50:06 +0200

    V0R0C2 Continue with the parser.
    '.' command added & completed.
    ',' command added & completed.
    '[' command added & completed.
    ']' command added but not completed.
    'showMemory' function added.
    Init memory changed: now 'memset' used.

--- src/test_inter.c
+++ src/test_inter.c
@@ -4,7 +4,9 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-#define MEM_SIZE 1024
+#define MEM_SIZE 15
+
+int i;
 
 int isBF(char *str) {
 	if (str[strlen(str)-3] == '.' && str[strlen(str)-2] == 'b' && str[strlen(str)-1] == 'f')
@@ -12,14 +14,35 @@ int isBF(char *str) {
 	return 1;
 }
 
+void showMemory (unsigned char * mem, int p) {
+	printf("Memory status:\n");
+	/*for (i = 0; i < MEM_SIZE; i++) {
+		printf("%d ", i);
+	}
+	printf("\n");*/
+	for (i = 0; i < MEM_SIZE; i++) {
+		printf("%c ", mem[i]);
+	}
+	/*char blanks [2*p];
+	memset(blanks, ' ', 2*p);
+	printf("\n%s*\n", blanks);*/
+	printf("\n");
+	for (i = 0; i < p; i++) {
+		printf("  ");
+	}
+	printf("*\n");
+}
+
 int main (int argc, char *argv[]) {
-	int fd, i;
+	int fd;
 	char c;
 	/* Init memory. This could be changed to be dynamic or argument passed */
 	unsigned char memory [MEM_SIZE];
 	int m_pointer; // Memory pointer
 
-	/* Check parameters */
+	/********************
+	 * Check parameters *
+	 *******************/
 	if (argc != 2) {
 		fprintf(stderr, "Error: Invalid number of arguments.\nExe form:\n\t%s <.bf file>\n", argv[0]);
 		return -1;
@@ -40,10 +63,10 @@ int main (int argc, char *argv[]) {
 
 	printf("Interpret file: %s\n", argv[1]);
 
-	/* Init memory */ // Memory could be initialliced in a dynamic form with all 0's
-	for (i = 0; i < MEM_SIZE; i++) {
-		memory[i] = 0;
-	}
+	/***************
+	 * Init memory * // Memory could be initialliced in a dynamic way with all 0's
+	 **************/
+	memset(memory, 'a', MEM_SIZE);
 	m_pointer = 0;
 
 	/* Read file */
@@ -58,6 +81,7 @@ int main (int argc, char *argv[]) {
 	 * ]	close while
 	 *
 	 */
+	//showMemory(memory, m_pointer);
 	while(read(fd, &c, 1) == 1) {
 		/* Conditions to parser 2 brainfuck */
 		if (c == '>')
@@ -68,9 +92,25 @@ int main (int argc, char *argv[]) {
 			memory[m_pointer] = (memory[m_pointer] + 1) % 255;
 		else if (c == '-')
 			memory[m_pointer] = (memory[m_pointer] - 1) % 255;
-		printf("MEM_INFO:\n\tm_pointer: %i\n\tvalue: %c\n", m_pointer, memory[m_pointer]);
+		else if (c == '.')
+			printf("%c", memory[m_pointer]);
+		else if (c == '[') {
+			// start while loop
+			if (memory[m_pointer] < 1) {
+				read(fd, &c, 1);
+				while (c != ']') {
+					read (fd, &c, 1);
+				}
+			}
+		} else if (c == ']') {
+			// end while loop
+			if (memory[m_pointer] > 0) {
+				// move pinter back to '['
+			}
+		}
+		//showMemory(memory, m_pointer);
 	}
-	
+	printf("\n");	
 	return 0;
 }
 
